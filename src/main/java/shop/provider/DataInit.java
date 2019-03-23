a546104d167a07cb7497149bee04e05a745ce22c
605355232@qq.com <dreams1234>
2019-03-23 21:09:17 +0800

    add

--- src/main/java/shop/provider/DataInit.java
+++ src/main/java/shop/provider/DataInit.java
@@ -14,7 +14,7 @@ public class DataInit {
 	
 	public boolean initConnect() {
 		dbpoolMgr = DBPoolMgr.getInst();
-		dbpoolMgr.init("192.168.2.120", 3306, "shop", "root", "123456", 30, 10);
+		dbpoolMgr.init("192.168.2.101", 2433, "shop", "root", "123456", 30, 10);
 		return true;
 	}
 	
